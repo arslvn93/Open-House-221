4c38f0f2a4866cb8170959147d0805f0969f48ff
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-05-22 18:56:59 -0400

    Change Realtor Details

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-const config = {
+{
   "property": {
-    "streetAddress": "142 Valley Glen Heights NW",
+    "streetAddress": "102, 10 Discovery Ridge Hill SW",
     "city": "Calgary",
-    "cityStateZip": "Calgary, AB T3B 5S8",
-    "heroSubtitle": "Amazing back yard, expansive 44x146-foot lot (7,588 sq ft) backing onto a tranquil nature reserve and the renowned Bow River Pathway. Register to receive the complete property Home &amp; Neighbourhood Info Package, virtual tour, floor plans, and comparables.",
-    "backgroundImageUrl": "https://cdn.realtor.ca/listing/TS638829328681570000/reb9/highres/1/A2219911_1.jpg"
+    "cityStateZip": "Calgary, AB T3H 5X2",
+    "heroSubtitle": "Corner upgraded walkout townhome that feels like an oasis outside of the city. Recent complex roof, premium location—directly across from parks, soccer fields, hockey rink, tennis/pickleball courts & playground, and just steps from Discovery Ridge Plaza & Wonder Donut Café. Register to receive the complete Home & Neighbourhood Info Package, virtual tour, floor plans, and comparables.",
+    "backgroundImageUrl": "https://unbranded.youriguide.com/102_10_discovery_ridge_hill_sw_calgary_ab/cover.jpg"
   },
   "realtor": {
     "fullName": "Amy Caughlin",
     "title": "REALTOR®",
     "phone": "15874391618",
-    "email": "arslan@salesgenius.co",
+    "email": "amy@caughlinreaestate.ca",
     "bio": "A former risk manager turned Realtor, proptech co-founder, podcast host, and passionate community advocate. I help busy professionals and families find homes that truly fit their lives, while working behind the scenes to make real estate better for everyone involved. With a background in risk management, I bring a strategic lens to every transaction, ensuring my clients make informed, confident decisions. I’m also the co-founder of Jazzberry, a platform designed by realtors, for realtors. A proud Maritimer, I lead with authenticity, hospitality, and heart. Whether I’m running along the Bow River, adventuring in the mountains with my family, or hosting FutureRE — The Future of Real Estate, I’m always driven by curiosity, connection, and impact.",
     "photoUrl": "https://static.showit.co/800/TUEDlhwVYAO5v1iU87fE1w/264146/headshot.jpg",
     "social": {
@@ -25,57 +25,30 @@ const config = {
     "address": "5940 Macleod Trl SW #500, Calgary, AB T2H 2G4"
   },
   "openHouse": {
-    "eventDate": "2025-05-17",
+    "eventDate": "2025-05-25",
     "bundleItems": [
-      {
-        "icon": "fas fa-ruler-combined",
-        "text": "Architectural Floor Plan"
-      },
-      {
-        "icon": "fas fa-star",
-        "text": "Neighbourhood Guide"
-      },
-      {
-        "icon": "fas fa-check-circle",
-        "text": "Market Stats"
-      },
-      {
-        "icon": "fas fa-car",
-        "text": "School District Information"
-      },
-      {
-        "icon": "fas fa-key",
-        "text": "Financing Resources"
-      },
-      {
-        "icon": "fas fa-home",
-        "text": "Utility Cost Estimates"
-      },
-      {
-        "icon": "fas fa-map-location-dot",
-        "text": "Property Feature List"
-      },
-      {
-        "icon": "fas fa-image",
-        "text": "Home Inspection Tips"
-      }
-    ]
+      { "icon": "fas fa-ruler-combined", "text": "Architectural Floor Plan", "link": "https://matrixmedia.pillarnine.com/mediaserver/GetMedia.ashx?Key=101000853&TableID=0&Type=3&Number=1&Size=0&NNF=1&RFN=RMS+-+102%2c+10+Discovery+Ridge+Hill+SW.pdf&tstmp=0522165012&exk=ed9b2841d31fae6dbc35369090b0e100" },
+      { "icon": "fas fa-star", "text": "Neighbourhood Guide" },
+      { "icon": "fas fa-check-circle", "text": "Market Stats" },
+      { "icon": "fas fa-car", "text": "School District Information" },
+      { "icon": "fas fa-key", "text": "Financing Resources" },
+      { "icon": "fas fa-home", "text": "Utility Cost Estimates" },
+      { "icon": "fas fa-map-location-dot", "text": "Property Feature List" },
+      { "icon": "fas fa-image", "text": "Home Inspection Tips" }
+    ],
+    "listingLink": "https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?ID=DE-266344908604&eml=YW15QGNhdWdobGlucmVhbGVzdGF0ZS5jYQ==#1",
+    "comparablesLink": "https://matrix.pillarnine.com/Matrix/Public/Portal.aspx?p=DE-266395716-759&k=4209874X4C2F&eml=YW15bGNhdWdobGluQGdtYWlsLmNvbQ==",
+    "virtualTourLink": "https://unbranded.youriguide.com/102_10_discovery_ridge_hill_sw_calgary_ab/"
   },
   "meta": {
     "pageTitlePrefix": "🏡 Amy Caughlin Open House",
-    "navBrandLogoText": "OPEN HOUSE",
+    "navBrandLogoText": "OPEN HOUSE 10 Discovery Ridge Hill SW",
     "privacyPolicyLink": "https://caughlinrealestate.ca/"
   },
   "settings": {
     "adminWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouseupdate",
     "visitorWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouse",
-    "confettiColors": [
-      "#6187a7",
-      "#f0e9d6",
-      "#ffffff",
-      "#83a363",
-      "#d9ac68"
-    ]
+    "confettiColors": ["#6187a7", "#f0e9d6", "#ffffff", "#83a363", "#d9ac68"]
   },
   "modalQuestions": [
     {
@@ -116,10 +89,10 @@ const config = {
     }
   ],
   "deploymentInfo": {
-    "repoName": "Open-House-957",
-    "repoUrl": "https://github.com/arslvn93/Open-House-957",
-    "tag": "Open House 142 Valley Glen Heights NW",
-    "netlifyUrl": "https://services.leadconnectorhq.com/hooks/zpjvSoJ3TMkdq8pcj7VJ/webhook-trigger/GIJI1X6L1zS4PxcScJqY",
-    "netlifyId": "dc35cadf-51e9-4ca8-843c-a084dd57add1"
+    "repoName": "Open-House-221",
+    "repoUrl": "https://github.com/arslvn93/Open-House-221",
+    "tag": "Open House 10 Discovery Ridge Hill SW",
+    "netlifyUrl": "http://Open-House-221.netlify.app",
+    "siteId": "988676048"
   }
-};
+}
